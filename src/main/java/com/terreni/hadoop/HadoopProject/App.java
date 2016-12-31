c40cfd6aebaf2b6f9487788c61112e7b00678d93
maurizioterreni <Maurizio.terreni@live.it>
2016-12-31 17:01:26 +0100

    FirstCommit

---
+++ src/main/java/com/terreni/hadoop/HadoopProject/App.java
@@ -0,0 +1,14 @@
+package com.terreni.hadoop.HadoopProject;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
